7897892f2129f4c4a7b279afc617d5eed713b41c
hyeyoungkim <velvetredvelvet91@gmail.com>
2022-02-24 17:23:29 +0900

    DeleteSuccessToast 추가

--- pages/components/FeedbackModal.js
+++ pages/components/FeedbackModal.js
@@ -20,6 +20,41 @@ const Container = styled.div`
 `
 
 const SavedToast = styled.div`
+  display: none;
+
+  width: 180px;
+  height: 50px;
+  background-color: green;
+  position: relative;
+  top: 70px;
+  left: 180px;
+  color: white;
+  border-radius: 5px;
+  text-align: left;
+  padding-left: 17px;
+  padding-top: 12px;
+
+  animation-name: fade;
+  animation-duration: 2s;
+  animation-timing-function: ease-in-out;
+  animation-fill-mode: forwards;
+
+  @keyframes fade {
+    0% {
+      opacity: 0;
+      transform: translate3d(0%, 0, 0);
+    }
+    50% {
+      opacity: 1;
+      transform: translateZ(0);
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+`
+
+const DeleteSuccessToast = styled.div`
   width: 180px;
   height: 50px;
   background-color: green;
@@ -92,6 +127,7 @@ const FeedbackModal = () => {
     <Wrapper>
       <Container>
         <SavedToast>저장이 완료되었습니다.</SavedToast>
+        <DeleteSuccessToast>삭제가 완료되었습니다.</DeleteSuccessToast>
         <NoMemoToast>메모를 입력해주세요.</NoMemoToast>
       </Container>
     </Wrapper>
