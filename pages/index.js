1cbc69251f7afc5c40ac89b5526d9e6a4ff4768e
starhn87 <starhn87@gmail.com>
2022-02-24 19:06:42 +0900

    FIX: 목록 아이템에서 모달 처리 방식 개선

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import styled from 'styled-components'
 import SearchBar from '../components/SearchBar'
-import Item from '../components/Item'
-import Image from 'next/image'
-import { useState } from 'react'
+import ItemGroup from '../components/item/ItemGroup'
 
 const Wrapper = styled.div`
   margin: auto;
@@ -12,67 +10,42 @@ const Wrapper = styled.div`
   background-color: white;
 `
 
-const ItemWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 20px 0;
-`
-
-const ImageWrapper = styled.div`
-  margin: 20px auto;
-  width: fit-content;
-
-  &:hover {
-    cursor: pointer;
-  }
-`
-
 const tempList = [
   {
-    name: '계명산자연휴양림',
-    addr: '충청북도 충주시 충주호수로 1170 (종민동)',
-    tel: '043-840-7930',
-  },
-  {
-    name: '계명산자연휴양림',
-    addr: '충청북도 충주시 충주호수로 1170 (종민동)',
-    tel: '043-840-7930',
-  },
-  {
+    id: 1,
     name: '계명산자연휴양림',
     addr: '충청북도 충주시 충주호수로 1170 (종민동)',
     tel: '043-840-7930',
     memo: '역시 계명산!',
   },
   {
+    id: 2,
     name: '계명산자연휴양림',
     addr: '충청북도 충주시 충주호수로 1170 (종민동)',
     tel: '043-840-7930',
+    memo: '역시 계명산!',
+  },
+  {
+    id: 3,
+    name: '2',
+    addr: '충청북도 충주시 충주호수로 1170 (종민동)',
+    tel: '043-840-7930',
+    memo: '역시 계명산!',
+  },
+  {
+    id: 4,
+    name: '3',
+    addr: '충청북도 충주시 충주호수로 1170 (종민동)',
+    tel: '043-840-7930',
+    memo: '역시 계명산!',
   },
 ]
 
 export default function Home() {
-  const [modal, setModal] = useState(false)
-
-  const onClick = () => {
-    console.log('modal open!')
-    setModal(true)
-  }
-
   return (
-    <>
-      <SearchBar></SearchBar>
-      <Wrapper>
-        <ItemWrapper>
-          {tempList.map((data, index) => (
-            <Item key={index} data={data} onClick={onClick} />
-          ))}
-        </ItemWrapper>
-        <ImageWrapper>
-          <Image src="/add.png" width={'30'} height={'30'} />
-        </ImageWrapper>
-      </Wrapper>
-    </>
+    <Wrapper>
+      <SearchBar />
+      <ItemGroup itemList={tempList} />
+    </Wrapper>
   )
 }
