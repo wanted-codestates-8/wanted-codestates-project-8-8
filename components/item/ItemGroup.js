31d3f4543532d08e0cf01874d5290d161d34d4c7
starhn87 <starhn87@gmail.com>
2022-02-25 10:53:19 +0900

    태그 변경시 로직 추가, 모달과 아이템 연동

--- components/item/ItemGroup.js
+++ components/item/ItemGroup.js
@@ -1,10 +1,5 @@
-import Image from 'next/image'
-import { useState } from 'react'
-import Modal from '../modal/Modal'
 import styled from 'styled-components'
 import Item from './Item'
-import Link from 'next/link'
-import Contents from '../Contents'
 
 const ItemWrapper = styled.div`
   display: flex;
@@ -22,36 +17,20 @@ const ItemContainer = styled.section`
   }
 `
 
-export default function ItemList({ itemList }) {
-  const [activedData, setActivedData] = useState(null)
-  const [close, setClose] = useState(true)
-
-  const closeModal = () => {
-    setActivedData(null)
-  }
+export default function ItemList({ itemList, onClick }) {
   return (
     <>
       <ItemWrapper>
         {itemList && itemList.length > 0 ? (
           itemList.map((data, index) => (
             <ItemContainer key={index}>
-              <Item data={data} onClick={() => setActivedData(data)} />
+              <Item data={data} onClick={() => onClick(data)} />
             </ItemContainer>
           ))
         ) : (
           <span>저장된 목록이 없습니다.</span>
         )}
       </ItemWrapper>
-      {close && activedData && (
-        <Modal onClose={closeModal}>
-          <Contents
-            id={activedData.id}
-            name={activedData.name}
-            addr={activedData.addr}
-            tel={activedData.tel}
-          />
-        </Modal>
-      )}
     </>
   )
 }
